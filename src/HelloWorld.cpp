acf509a1f947c47ad71b076e0165d313dbdde5eb
Timuçin Anuşlu <tiantik@gmail.com>
2010-12-09 14:37:13 +0200

    Bu bir Deneme Projesi olup master commit yapılacaktır.
    Signed-off-by: Timuçin Anuşlu <tiantik@gmail.com>

---
+++ src/HelloWorld.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : HelloWorld.cpp
+// Author      : Timuçin Anuşlu
+// Version     :
+// Copyright   : This program is DFU Programmer for USBK CryptoBridge
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "This program is DFU Programmer for USBK CryptoBridge" << endl; // prints This program is DFU Programmer for USBK CryptoBridge
+	return 0;
+}
